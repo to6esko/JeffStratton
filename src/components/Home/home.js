d5afe2a56f90bf5c526dc33eadf0727942e4f6f6
Todor Krastev <to6esko69@gmail.com>
2016-10-03 11:10:22 +0300

    add linck for home page

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Link, hashHistory} from 'react-router';
 
 export default class Home extends React.Component{
     render() {
         return (
             <div>
-                <h1>StrattonViolin</h1>                         
+               <h1>StrattonViolin</h1>     
             </div>
         );
     }
--- src/components/Violins/gypsy.js
+++ src/components/Violins/gypsy.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router';
+import Home from 'components/Home/home';
 
 export default class Gypsy extends React.Component{
     render() {
         return (
             <div>
-                <h1>StrattonViolin</h1>
+                <Link to="#/home"><h1>StrattonViolin</h1></Link>
                 <h2>Gypsy</h2>
             </div>
         );
