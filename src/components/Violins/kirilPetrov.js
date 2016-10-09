60627b05abe2ea92eb715d480f8c52408feadb7b
Todor Krastev <to6esko69@gmail.com>
2016-10-09 09:35:38 +0300

    add  text to violins

--- src/components/Violins/kirilPetrov.js
+++ src/components/Violins/kirilPetrov.js
@@ -12,7 +12,15 @@ export default class KirilPetrov extends React.Component{
                 </div>
                 <div className="kikiHome">
                     <Link to="#/home"><Home/></Link>
+                </div>
+                <div className="kikiContent">
                     <h2>Kiril Petrov</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.In eleifend ipsum leo.Fusce sit amet libero ac quam fringilla tempus nec nec massa.Cras euismod, dui varius posuere consequat, mi augue tristique nisl, eu mattis velit justo ac velit.Proin viverra quam vel aliquam lacinia.Quisque aliquet tincidunt faucibus.
+                    </p>
+                    <p>
+                        Cras id lorem nibh.Maecenas nec ornare nulla, ornare pretium dolor.Maecenas quis scelerisque nisl.Praesent efficitur, urna sit amet facilisis lacinia, elit odio porta leo, in pharetra neque odio nec elit.Praesent pretium ipsum mauris, et porttitor sapien varius in.Donec scelerisque risus quis fermentum finibus.Duis consectetur euismod vestibulum.
+                    </p>
                 </div>
             </div>
         );
--- src/components/Violins/robertKantor.js
+++ src/components/Violins/robertKantor.js
@@ -12,7 +12,15 @@ export default class RobertKantor extends React.Component{
                 </div>
                 <div className="kantorHome">
                     <Link to="#/home"><Home/></Link>
+                 </div>
+                <div className="kantorContent">
                     <h2>Robert Kantor</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.In eleifend ipsum leo.Fusce sit amet libero ac quam fringilla tempus nec nec massa.Cras euismod, dui varius posuere consequat, mi augue tristique nisl, eu mattis velit justo ac velit.Proin viverra quam vel aliquam lacinia.Quisque aliquet tincidunt faucibus.
+                    </p>
+                    <p>
+                        Cras id lorem nibh.Maecenas nec ornare nulla, ornare pretium dolor.Maecenas quis scelerisque nisl.Praesent efficitur, urna sit amet facilisis lacinia, elit odio porta leo, in pharetra neque odio nec elit.Praesent pretium ipsum mauris, et porttitor sapien varius in.Donec scelerisque risus quis fermentum finibus.Duis consectetur euismod vestibulum.
+                    </p>
                 </div>
             </div>
         );
