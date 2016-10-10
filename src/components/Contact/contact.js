b0a549fc7e2d6f119ced653c14c1dc4e093366d9
Todor Krastev <to6esko69@gmail.com>
2016-10-10 18:04:59 +0300

    add icons for contact

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -5,7 +5,10 @@ import Leault from 'components/leault';
 
 export default class Contact extends React.Component{
     render() {
-        var divStyle = { backgroundImage: 'url(' + '../../pages/contactLinks.jpg' + ')' };
+        let divStyle = { backgroundImage: 'url(' + '../../pages/contactLinks.jpg' + ')' };
+        let instaramImg = { backgroundImage: 'url(' + '../../icons/Instagram-icon.png' + ')', width: 25, height: 25 };
+        let faceBookImg = { backgroundImage: 'url(' + '../../icons/facebook-icon.png' + ')', width: 25, height: 25 };
+        let pinterestImg = { backgroundImage: 'url(' + '../../icons/pinterest-icon.png' + ')', width: 25,height:25};
         return (
             <div>
                 <div>
@@ -17,15 +20,30 @@ export default class Contact extends React.Component{
                 <div className="leaultContact">
                     <Leault/>
                 </div>
-                <div >
-                    <div>
-                        <h2>Links</h2>
+                <div className="contactList">
+                    <div className="contacts">
+                        <h2>Contact:</h2>
+                        <p>
+                            Phone: 760-444-4161
+                        </p>
+                        <p>
+                            Jeff Stratton
+                            </p>
+                        <p>
+                            Email: jeff@strattonviolin.com
+                            </p>
                     </div>
-                    <div>
-                        <h2>Contact</h2>
-                    </div>
-                    <div>
-                        <h2>Social</h2>
+                    <div className="social">
+                        <h2>Social:</h2>
+                        <p>
+                            <a href="https://www.facebook.com/Stratton-Violin-356861030998166/" style={faceBookImg} className="socialFaceBook" target="_blank">FaceBook</a>
+                            </p>
+                        <p>
+                            <a href="https://www.instagram.com/p/BBY5x-RNTGN/" style={instaramImg} className="socialInstagram" target="_blank">Instagram</a>
+                        </p>
+                        <p>
+                            <a href="https://www.pinterest.com/pin/458030224579731449/" style={pinterestImg} className="socialPinterest" target="_blank">Pinterest</a>
+                            </p>
                     </div>
                 </div>
             </div>
