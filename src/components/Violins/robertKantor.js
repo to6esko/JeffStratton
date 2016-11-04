351d666481c5ab34213592d503015c5b47966949
Todor Krastev <to6esko69@gmail.com>
2016-11-04 16:08:05 +0200

    add txt in kantor

--- src/components/Violins/robertKantor.js
+++ src/components/Violins/robertKantor.js
@@ -18,6 +18,9 @@ export default class RobertKantor extends React.Component{
                     <h3 className="price">The</h3>
                     <h3 className="price1">Robert Kantor Body</h3>
                     <p className="name">
+                        Kantor Guitars are famous for there work with Swarovski Elements.</p>
+                        <p className="name">  There work is perfect to say the least.</p>
+                    <p className="name">
                         These violins are custom order only.
                     </p>
                     <p className="name"> Contact me for more info.</p>
