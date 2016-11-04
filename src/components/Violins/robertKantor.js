983807f03fc2e5e1378124266b84b855918cb129
Todor Krastev <to6esko69@gmail.com>
2016-11-04 17:41:41 +0200

    small change2

--- src/components/Violins/robertKantor.js
+++ src/components/Violins/robertKantor.js
@@ -19,7 +19,7 @@ export default class RobertKantor extends React.Component{
                     <h3 className="price1">Robert Kantor Body</h3>
                     <p className="name">Kantor Guitars are famous for there work</p>
                     <p className="name">with Swarovski Elements.</p>
-                        <p className="name">There work is perfect to say the least.</p>
+                    <p className="name">There work is perfect to say the least.</p>
                     <p className="name">These violins are custom order only.</p>
                     <p className="name"> Contact me for more info.</p>
                 </div>
