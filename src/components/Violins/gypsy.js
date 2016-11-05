a07ac40b3562766636fdda5d16332bc6c70a766d
Todor Krastev <to6esko69@gmail.com>
2016-11-05 07:39:49 +0200

    change gypsy pic

--- src/components/Violins/gypsy.js
+++ src/components/Violins/gypsy.js
@@ -4,7 +4,7 @@ import Home from 'components/Home/home';
 
 export default class Gypsy extends React.Component{
     render() {
-        var divStyles = { backgroundImage: 'url(' + 'img/modelPagesOpt/gipsy.jpg' + ')' };
+        var divStyles = { backgroundImage: 'url(' + 'img/modelPagesOpt/gypsy2.jpg' + ')' };
         return (
             <div className="section-inner">
                 <div>
