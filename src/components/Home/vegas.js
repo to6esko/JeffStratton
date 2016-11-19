a9af710823fe2e2748bd26178335a4239b68d79f
Todor Krastev <to6esko69@gmail.com>
2016-11-19 13:59:18 +0200

    vegas remove img path duplicate

--- src/components/Home/vegas.js
+++ src/components/Home/vegas.js
@@ -7,35 +7,13 @@ export default class Vegas extends React.Component{
     componentDidMount() {  
         const browserUtil = new BrowserUtil();
 
-        const images = [
-            {
-                src: 'img/pageSlidersOpt/1.jpg'
-            },
-            {
-                src: 'img/pageSlidersOpt/2.jpg'
-            },
-            {
-                src: 'img/pageSlidersOpt/3.jpg'
-            },
-            {
-                src: 'img/pageSlidersOpt/4.jpg'
-            },
-            {
-                src: 'img/pageSlidersOpt/5.jpg'
-            },
-            {
-                src: 'img/pageSlidersOpt/6.jpg'
-            },
-            {
-                src: 'img/pageSlidersOpt/7.jpg'
-            },
-            {
-                src: 'img/pageSlidersOpt/8.jpg'
-            },
-            {
-                src: 'img/pageSlidersOpt/9.jpg'
-            }
-        ];       
+        let images = [];
+            
+        for (let i of Array(8).keys()) {
+            images.push({
+                src: `img/pageSlidersOpt/${i + 1}.jpg`
+            });
+        }
 
         const convertedImages = images.map(image => {
             image.src = browserUtil.convertImagePath(image.src);
