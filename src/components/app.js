852f5a440c3f73be3f050ea923e1dd251de4943d
Todor Krastev <to6esko69@gmail.com>
2017-02-14 08:42:01 +0200

    change sideshow

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ import Leault from './leault';
 import Home from 'components/Home/home';
 //import SlideShow from 'components/Home/pageSlider';
 import Sliders from 'components/Home/sliders';
-import Vegas from 'components/Home/vegas';
+//import Vegas from 'components/Home/vegas';
 
 export default class App extends React.Component{
     constructor() {
